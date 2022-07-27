2b0db49d0773ca9654f60aae7c0aa5823a12befe
hagen-gloetter <38193521+hagen-gloetter@users.noreply.github.com>
2022-07-27 22:06:15 +0200

    guezli compression added and fixed

--- hg_watermark_it.sh
+++ hg_watermark_it.sh
@@ -5,13 +5,12 @@
 # brew install imagemagick
 # brew install guetzli
 
-
 _self="${0##*/}"
 echo "$_self is called"
 
-if [[ $# -eq 0 ]] ; then
-    echo "Usage: $(basename $0) [foldername]" 
-    exit 1
+if [[ $# -eq 0 ]]; then
+  echo "Usage: $(basename $0) [foldername]"
+  exit 1
 fi
 
 ## or in usage() ##
@@ -22,9 +21,16 @@ shopt -s nullglob
 # use the nullglob option to simply ignore a failed match and not enter the body of the loop.
 COMPOSITE=$(which composite) # path to imagemagick compose
 CONVERT=$(which convert)
+QUALITYJPG="85"
 GUETZLI=$(which guetzli)
-QUALITY="75"
-cat /etc/issue | grep -i "ubuntu"
+GUETZLI_EXISTS="NO"
+if [ $? -eq 0 ]; then
+  echo "Guezli Compression found"
+  GUETZLI_EXISTS="YES"
+  QUALITYGZLY=$QUALITYJPG
+  QUALITYJPG=100 # render jpgs 100% quality and do compression by guezli
+fi
+UBUNTU=$(cat /etc/issue | grep -i "ubuntu")
 if [ $? -eq 0 ]; then
   echo "Ubuntu detected"
   DIR_SCRIPT=$(dirname $(readlink -f $0))
@@ -89,13 +95,19 @@ check_DIR $DIR_WATERMARK_IMAGES
 
 #DIR_BASE=`realpath $1`  # works
 ## SE
-WATERMARK_SE_L="$DIR_WATERMARK_IMAGES/gloetter_de_wasserzeichen_1600px.png" ; echo "WATERMARK_SE_L = $WATERMARK_SE_L"
-WATERMARK_SE_M="$DIR_WATERMARK_IMAGES/gloetter_de_wasserzeichen_1100px.png" ; echo "WATERMARK_SE_M = $WATERMARK_SE_M"
-WATERMARK_SE_S="$DIR_WATERMARK_IMAGES/gloetter_de_wasserzeichen_500px.png"  ; echo "WATERMARK_SE_S = $WATERMARK_SE_S"
+WATERMARK_SE_L="$DIR_WATERMARK_IMAGES/gloetter_de_wasserzeichen_1600px.png"
+echo "WATERMARK_SE_L = $WATERMARK_SE_L"
+WATERMARK_SE_M="$DIR_WATERMARK_IMAGES/gloetter_de_wasserzeichen_1100px.png"
+echo "WATERMARK_SE_M = $WATERMARK_SE_M"
+WATERMARK_SE_S="$DIR_WATERMARK_IMAGES/gloetter_de_wasserzeichen_500px.png"
+echo "WATERMARK_SE_S = $WATERMARK_SE_S"
 ## SW
-WATERMARK_SW_L="$DIR_WATERMARK_IMAGES/Sternwarte-Wasserzeichen_1680x580px.png" ; echo "WATERMARK_SW_L = $WATERMARK_SW_L"
-WATERMARK_SW_M="$DIR_WATERMARK_IMAGES/Sternwarte-Wasserzeichen_1000x290px.png" ; echo "WATERMARK_SW_M = $WATERMARK_SW_M"
-WATERMARK_SW_S="$DIR_WATERMARK_IMAGES/Sternwarte-Wasserzeichen_500x150px.png"; echo "WATERMARK_SW_S = $WATERMARK_SW_S"
+WATERMARK_SW_L="$DIR_WATERMARK_IMAGES/Sternwarte-Wasserzeichen_1680x580px.png"
+echo "WATERMARK_SW_L = $WATERMARK_SW_L"
+WATERMARK_SW_M="$DIR_WATERMARK_IMAGES/Sternwarte-Wasserzeichen_1000x290px.png"
+echo "WATERMARK_SW_M = $WATERMARK_SW_M"
+WATERMARK_SW_S="$DIR_WATERMARK_IMAGES/Sternwarte-Wasserzeichen_500x150px.png"
+echo "WATERMARK_SW_S = $WATERMARK_SW_S"
 
 # create subfolders for images
 DIR_WATERMARK=$DIR_BASE"/watermarked"
@@ -148,14 +160,13 @@ for FN in *.jpg *.jpeg *.JPG *.JPEG; do
     WATERMARK_SE=$WATERMARK_SE_S
   fi
   # composite -gravity SouthEast gloetter_de_wasserzeichen_1100px.png IMG_6269.JPG Test2.jpg
-  # TODO set both Watermarks at once if possible
   TRANDPARENZ="-dissolve 50%"
-#  TRANDPARENZ=""
+  #  TRANDPARENZ=""
   # OFFSET_WATERMARK_X=0 # debug
   CMD="$COMPOSITE -gravity SouthWest -geometry +"$OFFSET_WATERMARK_X"+"$OFFSET_WATERMARK_Y" $TRANDPARENZ \( \"$WATERMARK_SW\"  \) \"$FN\" \"$DIR_WATERMARK_6k/$FN\""
   echo "processing: - >$FN< -- CMD: $CMD"
   eval $CMD
-  # TODO set gloetter watermark only if filename containd "HG"
+  # set gloetter watermark only if filename containd "HG"
   case "$FN" in *HG*)
     echo "HG found in filename $FN"
     CMD="$COMPOSITE -gravity SouthEast -geometry +"$OFFSET_WATERMARK_X"+"$OFFSET_WATERMARK_Y" $TRANDPARENZ \( \"$WATERMARK_SE\"  \) \"$DIR_WATERMARK_6k/$FN\" \"$DIR_WATERMARK_6k/$FN\""
@@ -165,16 +176,34 @@ for FN in *.jpg *.jpeg *.JPG *.JPEG; do
   *) ;;
   esac
   # TODO convert all sizes here maybe via loop
-  
-    # 4k
-    CMD="$CONVERT \"$DIR_WATERMARK_6k/$FN\" -resize $r4k -strip -quality $QUALITY  \"$DIR_WATERMARK_4k/$FN\" "
-    echo -e "resizing: - >$FN< -- CMD: $CMD\n"
+  # 4k
+  CMD="$CONVERT \"$DIR_WATERMARK_6k/$FN\" -resize $r4k -strip -quality $QUALITYJPG  \"$DIR_WATERMARK_4k/$FN\" "
+  echo -e "resizing: - >$FN< -- CMD: $CMD\n"
+  eval $CMD
+  # 2k
+  CMD="$CONVERT  \"$DIR_WATERMARK_6k/$FN\" -resize $r2k -strip -quality $QUALITYJPG  \"$DIR_WATERMARK_2k/$FN\""
+  echo -e "resizing: - >$FN< -- CMD: $CMD\n"
+  eval $CMD
+  #  use guezli compression for jpgs for smaller filesizes
+  if [ $GUETZLI_EXISTS == "YES" ]; then
+    echo "Guezli compression = on"
+    # compress images for web
+    # basic syntax:
+    # guetzli --quality 85 image.jpg image-out.jpg
+    FNW="web_"$FN
+    #    echo "Guezli 6k"
+    #    echo $CMD
+    CMD="$GUETZLI --quality $QUALITYGZLY  \"$DIR_WATERMARK_6k/$FN\" \"$DIR_WATERMARK_4k/$FNW\"  "
+    echo "$CMD" >> ../guezli_6k_list.sh
+    echo "Guezli 4k"
+    CMD="$GUETZLI --quality $QUALITYGZLY  \"$DIR_WATERMARK_4k/$FN\" \"$DIR_WATERMARK_4k/$FNW\"  "
+    #    echo $CMD
     eval $CMD
-    # 2k
-    CMD="$CONVERT  \"$DIR_WATERMARK_6k/$FN\" -resize $r2k -strip -quality $QUALITY  \"$DIR_WATERMARK_2k/$FN\""
-    echo -e "resizing: - >$FN< -- CMD: $CMD\n"
+    echo "Guezli 2k"
+    CMD="$GUETZLI --quality $QUALITYGZLY  \"$DIR_WATERMARK_2k/$FN\" \"$DIR_WATERMARK_2k/$FNW\"  "
+    #    echo $CMD
     eval $CMD
-    # TODO use brotli compression for jpgs for smaller filesizes
+  fi
 done
 
 after=$(date +%s)
@@ -184,19 +213,3 @@ echo $RT
 echo $RT >script_execution_time.txt
 
 exit
-
-# resize images for web
-
-  # basic syntax:
-  # guetzli --quality 85 image.jpg image-out.jpg
-  FNW="web_"$FN
-  CMD="$GUETZLI --quality 85 \"$DIR3/$FN\" \"$DIR3/$FNW\" "
-  echo "compressing: - >$FN< -- CMD: $CMD"
-  eval $CMD
-done
-cd ..
-after=$(date +%s)
-runtime=$((after - $before))
-RT="elapsed time: $runtime seconds"
-echo $RT
-echo $RT >script_execution_time.txt
